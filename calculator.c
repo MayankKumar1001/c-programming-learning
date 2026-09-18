ee7a0d711c92ec3aa9bb07bb5b80d2756e2724c1
Mayank-Kumar10 <mayank10kumar1001@gmail.com>
2026-09-18 23:57:31 +0530

    added exponent operation

--- calculator.c
+++ calculator.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int a,b,sum,diff,product,division;
+    int a,b,sum,diff,product,division,power;
     char symbol;
     printf("Enter two numbers : ");
     scanf("%d%d",&a,&b);
     printf("Enter the operation required : ");
     scanf(" %c",&symbol);
-
+  
+    {
     switch (symbol)
     {
     case 42 : product= a*b;
@@ -24,10 +26,15 @@ int main()
     case 47 : division = a/b;
                 printf("%d/%d = %d",a,b,division);
                 break;
+    case 94 : power = pow(a,b);
+                printf("%d ^ %d = %d",a,b,power);
+                break;
                 
     default : printf("Invalid Choice.");
+                break;
+                
     
 return 0;
-
+    }
 }
 }
