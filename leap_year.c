0deeb7f98e1eb2f6a78d582345dd845b2b7cb0eb
Mayank-Kumar10 <mayank10kumar1001@gmail.com>
2026-09-26 02:06:48 +0530

    fixed the error

--- leap_year.c
+++ leap_year.c
@@ -4,9 +4,21 @@ int main()
     int year;
     printf("enter the year : ");
     scanf("%d",&year);
+    
     if(year%4==0)
-    printf("%d is a leap year.",year);
-    else
-    printf("%d is not a leap year.",year);
+    {
+        if(year%400==0)
+        {
+            printf("this is a leap year.");
+        }
+        else if(year%100==0)
+        {
+            printf("this is not a leap year.");
+        }
+        else
+        {
+            printf("this is a leap year.");
+        }
+    }
     return 0;
 }
